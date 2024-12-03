c0b8e1cb6d12505602790ec1947376b226d1e359
Данила FindTheRhythm <140545017+FindTheRhythm@users.noreply.github.com>
2024-12-04 02:42:58 +0300

    Update Main.java

--- docker_maven/src/main/java/com/example/Main.java
+++ docker_maven/src/main/java/com/example/Main.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from Maven Container!");
+        System.out.println("Hello, Maven from Docker Container!");
     }
 }
