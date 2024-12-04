654cf6957522332f1a8bf72fc6b6f2f74b87c0d3
Данила FindTheRhythm <140545017+FindTheRhythm@users.noreply.github.com>
2024-12-04 09:21:18 +0300

    Update App.java

--- docker_maven/src/main/java/App.java
+++ docker_maven/src/main/java/App.java
@@ -1,5 +1,25 @@
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello, Maven in Docker!");
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Получение порта из переменной среды
+        String port = System.getenv("PORT");
+        if (port == null) {
+            port = "8080"; // Укажите порт по умолчанию
+        }
+
+        // Настройка HTTP-сервера
+        HttpServer server = HttpServer.create(new InetSocketAddress(Integer.parseInt(port)), 0);
+        server.createContext("/", exchange -> {
+            String response = "Hello, Maven in Docker!";
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            exchange.getResponseBody().write(response.getBytes());
+            exchange.close();
+        });
+
+        System.out.println("Server is running on port " + port);
+        server.start();
     }
 }
